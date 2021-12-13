0893de964f6e19f674130a01b882a6c7d19ed78c
MAFIA <mosaabmograbe@yahoo.com>
2021-12-13 22:38:08 +0200

    fetch api data

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Testing');
+//Unsplash API
+const count = 10;
+const apiKey = `3kSVx2dKk9nm4g02HwsCEToXUiHDb4RuUtxPXYAKiho`;
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+//get photo from unsplash api
+async function getPhoto() {
+     try{
+         const response = await fetch(apiUrl);
+         const data = await response.json();
+         console.log(data);
+     }catch(error){
+
+     }
+}
+//on Load
+getPhoto();
