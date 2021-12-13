5d769996713a73e5f3d21512cd1ddc93f72387f8
MAFIA <mosaabmograbe@yahoo.com>
2021-12-13 23:27:57 +0200

    display  data

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+const imgContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let photoArray = [] ;
+
+//create element for links,photo add to DOM
+function displayPhoto(){
+    //run function forEach
+    photoArray.forEach((photo) =>{
+        //create <a> to link unsplash
+        const item = document.createElement('a');
+        item.setAttribute('href',photo.links.html);
+        item.setAttribute('target','_blank');
+        console.log(photo.links.html);
+        //create img for photo 
+        const img = document.createElement('img');
+        img.setAttribute('src',photo.urls.regular);
+        img.setAttribute('atl',photo.alt_description);
+        img.setAttribute('title',photo.alt_description);
+        console.log(photo.urls.regular);
+        //put <img> inside <a>,then put both inside imgContanier
+        item.appendChild(img);
+        imgContainer.appendChild(item); 
+    })
+}
 //Unsplash API
 const count = 10;
 const apiKey = `3kSVx2dKk9nm4g02HwsCEToXUiHDb4RuUtxPXYAKiho`;
@@ -7,10 +32,10 @@ const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&coun
 async function getPhoto() {
      try{
          const response = await fetch(apiUrl);
-         const data = await response.json();
-         console.log(data);
+         const photoArray = await response.json();
+         displayPhoto();
      }catch(error){
-
+          //to catch errors
      }
 }
 //on Load
